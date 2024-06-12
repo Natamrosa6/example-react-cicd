9411fcd07038fd1ba0d4ba6f42d6699ac67f8cd1
Natalia Tamara  Rosa <109100346+Natamrosa6@users.noreply.github.com>
2024-06-12 12:31:20 +0200

    testing exercises

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -33,3 +33,4 @@ test('adds 1 and 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
+
--- src/helpers/getInfo.js
+++ src/helpers/getInfo.js
@@ -7,3 +7,17 @@ export const getInfo = (object) => {
         return 'none'
     }
 }
+
+test('returns "none" when null is passed in', () => {
+    expect(getInfo(null)).toBe('none');
+});
+
+
+test('joins names correctly', () => {
+    const mockObject = {
+        language1: { name: 'English' },
+        language2: { name: 'Spanish' }
+    };
+    expect(getInfo(mockObject)).toBe('English, Spanish');
+});
+ 
